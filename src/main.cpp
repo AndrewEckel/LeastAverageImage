601ad949b62bce309b3f9a2af483eec8ccdb3363
Andrew Eckel <ALEckel@gmail.com>
2020-05-05 20:01:16 -0400

    - Version 0.05, 2019-07-07     - (I have absolutely no idea why there is no version 0.04.) - Created before the git repo, no commit message

--- src/main.cpp
+++ src/main.cpp
@@ -119,26 +119,38 @@ double difference_Combined(Pixel p1, Pixel p2){
 
 int main()
 {
-	std::cout << "LeastAverageImage Version 0.03\n";
+	std::cout << "LeastAverageImage Version 0.05\n";
 
 	//Constants for now, could be inputs in a later version of this program.
-	const std::string INPUT_PATH = "G:\\LeastAverageImage project 2 (A610)\\";
-	const std::string SCENE_NAME = "a610_";
-	const int FIRST_FRAME = 0;
-	const int LAST_FRAME = 10316;
+	const std::string INPUT_PATH = "G:\\LeastAverageImage inputs\\Castle Island\\main set\\";
+	const std::string SCENE_NAME = "castleisland_";
+	const int FIRST_FRAME = 1;
+	const int LAST_FRAME = 408;
 	const int INPUT_NUM_DIGITS = 6;
 	const std::string OUTPUT_PATH = "";
+	const bool SKIP_AVERAGING_PHASE = false;
+	const int NUM_PIXELS_TO_RANK = 12;
+	const bool COUNTDOWN = true;
+	//const bool SQUARE_SCORE = false;
+	const bool INVERT_SCORES = false;
 
-	std::stringstream ss0;
-	ss0 << std::setw(INPUT_NUM_DIGITS) << std::setfill('0') << FIRST_FRAME << "-" << LAST_FRAME;
-	const std::string OUTPUT_TAG = SCENE_NAME + ss0.str();
-
+	std::vector<double> powersOfScore;
+	powersOfScore.push_back(1.0);
+	powersOfScore.push_back(2.0);
+	powersOfScore.push_back(2.5);
+	powersOfScore.push_back(3.5);
+	
 	//Really constants
 	const int NUM_COLOR_CHANNELS = 3; //R,G,B.
 	const int RED_INDEX = 0;
 	const int GREEN_INDEX = 1;
 	const int BLUE_INDEX = 2;
 	const int CHAR_LENGTH_LEEWAY = 100;
+
+	std::stringstream ss0;
+	ss0 << std::setw(INPUT_NUM_DIGITS) << std::setfill('0') << FIRST_FRAME << "-" << LAST_FRAME;
+	const std::string OUTPUT_TAG = SCENE_NAME + ss0.str();
+
 
 	int numImages = 1 + LAST_FRAME - FIRST_FRAME;
 	char* filenameWithPath = (char*) malloc(INPUT_PATH.length() + SCENE_NAME.length() + INPUT_NUM_DIGITS + 4 + CHAR_LENGTH_LEEWAY);
@@ -152,53 +164,64 @@ int main()
 	// }	
 
 	//First pass: Sum all the values in the input files.
-	std::cout << "Beginning averaging phase. First image should take the longest.\n";
-	std::vector<std::vector<std::vector< unsigned long long > > > totals;
-	//First image
+	Image meanAverageImage;
 	sprintf(filenameWithPath, "%s%s%0*d.ppm", INPUT_PATH.c_str(), SCENE_NAME.c_str(), INPUT_NUM_DIGITS, FIRST_FRAME);
 	Image img = readImage(filenameWithPath);
 	int height = img.height;
 	int width = img.width;
-	for(int i = 0; i < height; ++i){
-		totals.push_back(std::vector<std::vector<unsigned long long > >());
-		for(int j = 0; j < width; ++j){
-			totals[i].push_back(std::vector<unsigned long long >());
-			totals[i][j].push_back(img.map[i][j].r); //0
-			totals[i][j].push_back(img.map[i][j].g); //1
-			totals[i][j].push_back(img.map[i][j].b); //2
-		}
+	
+	if(SKIP_AVERAGING_PHASE){
+		std::cout << "SKIPPING AVERAGING PHASE. Reading in pre-averaged file.\n";
+		meanAverageImage = readImage("C:\\Code\\LeastAverageImage\\input_averages\\ive_eddy_000000-152avg.ppm");
+		deleteImage(img);
 	}
-	deleteImage(img);
-	std::cout << "Averaging: Processed 1st image ok\n";
-	for(int x = FIRST_FRAME + 1; x <= LAST_FRAME; ++x){ //starting loop with 2nd image because we already did the first as a special case.
-		sprintf(filenameWithPath, "%s%s%0*d.ppm", INPUT_PATH.c_str(), SCENE_NAME.c_str(), INPUT_NUM_DIGITS, x);
-		Image img = readImage(filenameWithPath);
+	else{
+		std::cout << "Beginning averaging phase. First image should take the longest.\n";
+		std::vector<std::vector<std::vector< unsigned long long > > > totals;
+		//First image
 		
-		if(img.height != height || img.width != width){
-			std::cerr << "ERROR: Image #" << x << " dimensions do not match those of image #" << FIRST_FRAME << "!\n";
-			deleteImage(img);
-			exit(1);
-		}
-
 		for(int i = 0; i < height; ++i){
+			totals.push_back(std::vector<std::vector<unsigned long long > >());
 			for(int j = 0; j < width; ++j){
-				totals[i][j][RED_INDEX] += img.map[i][j].r;
-				totals[i][j][GREEN_INDEX] += img.map[i][j].g;
-				totals[i][j][BLUE_INDEX] += img.map[i][j].b;
+				totals[i].push_back(std::vector<unsigned long long >());
+				totals[i][j].push_back(img.map[i][j].r); //0
+				totals[i][j].push_back(img.map[i][j].g); //1
+				totals[i][j].push_back(img.map[i][j].b); //2
 			}
 		}
 		deleteImage(img);
-		std::cout << "Averaging: Processed image #" << (x - FIRST_FRAME + 1) << " of " << numImages << "\n";
-	}
-	Image meanAverageImage = createImage(height, width);
-	for(int i = 0; i < height; ++i){
-		for(int j = 0; j < width; ++j){
-			meanAverageImage.map[i][j].r = (unsigned char) round(1.0 * totals[i][j][RED_INDEX] / numImages);
-			meanAverageImage.map[i][j].g = (unsigned char) round(1.0 * totals[i][j][GREEN_INDEX] / numImages);
-			meanAverageImage.map[i][j].b = (unsigned char) round(1.0 * totals[i][j][BLUE_INDEX] / numImages);
+		
+		std::cout << "Averaging: Processed 1st image ok\n";
+		for(int x = FIRST_FRAME + 1; x <= LAST_FRAME; ++x){ //starting loop with 2nd image because we already did the first as a special case.
+			sprintf(filenameWithPath, "%s%s%0*d.ppm", INPUT_PATH.c_str(), SCENE_NAME.c_str(), INPUT_NUM_DIGITS, x);
+			Image img = readImage(filenameWithPath);
+			
+			if(img.height != height || img.width != width){
+				std::cerr << "ERROR: Image #" << x << " dimensions do not match those of image #" << FIRST_FRAME << "!\n";
+				deleteImage(img);
+				exit(1);
+			}
+
+			for(int i = 0; i < height; ++i){
+				for(int j = 0; j < width; ++j){
+					totals[i][j][RED_INDEX] += img.map[i][j].r;
+					totals[i][j][GREEN_INDEX] += img.map[i][j].g;
+					totals[i][j][BLUE_INDEX] += img.map[i][j].b;
+				}
+			}
+			deleteImage(img);
+			std::cout << "Averaging: Processed image #" << (x - FIRST_FRAME + 1) << " of " << numImages << "\n";
 		}
+		meanAverageImage = createImage(height, width);
+		for(int i = 0; i < height; ++i){
+			for(int j = 0; j < width; ++j){
+				meanAverageImage.map[i][j].r = (unsigned char) round(1.0 * totals[i][j][RED_INDEX] / numImages);
+				meanAverageImage.map[i][j].g = (unsigned char) round(1.0 * totals[i][j][GREEN_INDEX] / numImages);
+				meanAverageImage.map[i][j].b = (unsigned char) round(1.0 * totals[i][j][BLUE_INDEX] / numImages);
+			}
+		}
+		writeImage(meanAverageImage, OUTPUT_PATH + OUTPUT_TAG + "avg.ppm");
 	}
-	writeImage(meanAverageImage, OUTPUT_PATH + OUTPUT_TAG + "avg.ppm");
 
 	//Second pass: Find the most different.
 	std::cout << "Beginning differenciating phase. First image should take the longest.\n";
@@ -207,14 +230,41 @@ int main()
 	Image mostDifferentImage_regular = createImage(height, width);
 	std::vector<std::vector<double > > biggestRegularDifference(height, std::vector<double>(width, 0));
 	//Variables using color ratio difference:
-	Image mostDifferentImage_colorRatio = createImage(height, width);
-	std::vector<std::vector<double > > biggestRatioDifference(height, std::vector<double>(width, 0));
-	//Variables using perceived brightness difference:
+	// Image mostDifferentImage_colorRatio = createImage(height, width);
+	std::vector<std::vector<std::vector<Pixel > > > mostDifferentPixels_ColorRatio(height, std::vector<std::vector<Pixel > >(width, std::vector<Pixel>(NUM_PIXELS_TO_RANK, Pixel())));
+	//std::vector<std::vector<std::vector<Pixel > > > mostDifferentPixels_ColorRatio;
+	//std::vector<std::vector<std::vector<Pixel > > > mostDifferentPixels_ColorRatio(height, std::vector<std::vector<Pixel > >(width, std::vector<Pixel>));
+	std::vector<std::vector<std::vector<double > > > biggestRatioDifference(height, std::vector<std::vector<double > >(width, std::vector<double>(NUM_PIXELS_TO_RANK, 0)));
+	
+	for(int i = 0; i < height; ++i){
+		mostDifferentPixels_ColorRatio.push_back(std::vector<std::vector<Pixel > >());
+		for(int j = 0; j < width; ++j){
+			mostDifferentPixels_ColorRatio[i].push_back(std::vector<Pixel>());
+			for(int k = 0; k < NUM_PIXELS_TO_RANK; ++k){
+				Pixel blah;
+				blah.r = 255;
+				blah.g = 255;
+				blah.b = 255;
+				mostDifferentPixels_ColorRatio[i][j].push_back(blah);
+			}
+		}
+	}
+
+
+	// //Variables using perceived brightness difference:
 	Image mostDifferentImage_Bright = createImage(height, width);
 	std::vector<std::vector<double > > biggestBrightDifference(height, std::vector<double>(width, 0));
-	//Variables using combined difference:
+	// //Variables using combined difference:
 	Image mostDifferentImage_Combined = createImage(height, width);
 	std::vector<std::vector<double > > biggestCombinedDifference(height, std::vector<double>(width, 0));
+
+	// mostDifferentPixels_ColorRatio[0][3][0].r = 111;
+	// mostDifferentPixels_ColorRatio[0][3][0].g = 111;
+	// mostDifferentPixels_ColorRatio[0][3][0].b = 111;
+	// for(int j = 0; j < 5; ++j){
+	// 	std::cout << "(0," << j << "): (" << mostDifferentPixels_ColorRatio[0][j][0].r << ", " << mostDifferentPixels_ColorRatio[0][j][0].g << ", " << mostDifferentPixels_ColorRatio[0][j][0].b << ")\n";
+	// }
+
 
 	for(int x = FIRST_FRAME; x <= LAST_FRAME; ++x){
 		sprintf(filenameWithPath, "%s%s%0*d.ppm", INPUT_PATH.c_str(), SCENE_NAME.c_str(), INPUT_NUM_DIGITS, x);
@@ -230,9 +280,27 @@ int main()
 					biggestRegularDifference[i][j] = diff_regular;
 					copyPixel(&mostDifferentImage_regular.map[i][j], &img.map[i][j]);
 				}
-				if(diff_colorRatio > biggestRatioDifference[i][j]){
-					biggestRatioDifference[i][j] = diff_colorRatio;
-					copyPixel(&mostDifferentImage_colorRatio.map[i][j], &img.map[i][j]);
+				// if(diff_colorRatio > biggestRatioDifference[i][j]){
+				// 	biggestRatioDifference[i][j] = diff_colorRatio;
+				// 	copyPixel(&mostDifferentImage_colorRatio.map[i][j], &img.map[i][j]);
+				// }
+				if(diff_colorRatio > biggestRatioDifference[i][j][NUM_PIXELS_TO_RANK - 1]){
+					int rank = NUM_PIXELS_TO_RANK - 1;
+					while(rank >= 0 && diff_colorRatio > biggestRatioDifference[i][j][rank]){
+						--rank;
+					}
+					++rank;
+					//std::cout << "Rank of " << rank << "\n";
+					if(rank < 0 || rank >= NUM_PIXELS_TO_RANK){
+						std::cerr << "ERROR: rANK = " << rank << " for (" << i << ", " << j << ")\n";
+						exit(1);
+					}
+					for(size_t backwards_iterator = NUM_PIXELS_TO_RANK - 1; backwards_iterator > rank; --backwards_iterator){
+						biggestRatioDifference[i][j][backwards_iterator] = biggestRatioDifference[i][j][backwards_iterator - 1];
+						copyPixel(&mostDifferentPixels_ColorRatio[i][j][backwards_iterator], &mostDifferentPixels_ColorRatio[i][j][backwards_iterator - 1]);
+					}
+					biggestRatioDifference[i][j][rank] = diff_colorRatio;
+					copyPixel(&mostDifferentPixels_ColorRatio[i][j][rank], &img.map[i][j]);
 				}
 				if(diff_bright > biggestBrightDifference[i][j]){
 					biggestBrightDifference[i][j] = diff_bright;
@@ -247,13 +315,76 @@ int main()
 		deleteImage(img);
 		std::cout << "Differenciating: Processed image #" << (x - FIRST_FRAME + 1) << " of " << numImages << "\n";
 	}
+
+	// double score_power = 1.0;
+	// if(SQUARE_SCORE){
+	// 	score_power = 2.0;
+	// }
+
+	int lastloop = 0;
+	if(COUNTDOWN){
+		lastloop = NUM_PIXELS_TO_RANK - 1;
+	}
+	for(int rank_this_many_fewer = 0; rank_this_many_fewer <= lastloop; ++rank_this_many_fewer){
+		int num_pixels_to_rank_this_round = NUM_PIXELS_TO_RANK - rank_this_many_fewer;
+		for(size_t score_power_index = 0; score_power_index < powersOfScore.size(); ++score_power_index){
+			Image mostDifferentImage_colorRatio = createImage(height, width);
+			for(int i = 0; i < height; ++i){
+				for(int j = 0; j < width; ++j){
+					double totalScore = 0.0;
+					for(int k = 0; k < num_pixels_to_rank_this_round; ++k){
+						totalScore += pow(biggestRatioDifference[i][j][k], powersOfScore[score_power_index]);
+					}
+					if(totalScore == 0.0){
+						std::cout << "WARNING: All pixels at position " << i << ", " << j << " are equal to the average.\n";
+						copyPixel(&mostDifferentImage_colorRatio.map[i][j], &meanAverageImage.map[i][j]);
+					}
+					else{
+						//std::vector<double> weights;
+						std::vector<double> newRGB(NUM_COLOR_CHANNELS, 0.0);
+						for(int k = 0; k < num_pixels_to_rank_this_round; ++k){
+							//weights.push_back(biggestRatioDifference[i][j][k] / totalScore);
+							double weight = pow(biggestRatioDifference[i][j][k], powersOfScore[score_power_index]) / totalScore;
+							if(INVERT_SCORES){
+								weight = 1 - weight;
+							}
+							newRGB[RED_INDEX] += mostDifferentPixels_ColorRatio[i][j][k].r * weight;
+							newRGB[GREEN_INDEX] += mostDifferentPixels_ColorRatio[i][j][k].g * weight;
+							newRGB[BLUE_INDEX] += mostDifferentPixels_ColorRatio[i][j][k].b * weight;
+						}
+						mostDifferentImage_colorRatio.map[i][j].r = (unsigned char) round(newRGB[RED_INDEX]);
+						mostDifferentImage_colorRatio.map[i][j].g = (unsigned char) round(newRGB[GREEN_INDEX]);
+						mostDifferentImage_colorRatio.map[i][j].b = (unsigned char) round(newRGB[BLUE_INDEX]);
+					}
+				}
+			}
+			std::stringstream ss1;
+			ss1 << num_pixels_to_rank_this_round;
+			std::string numPixelsToRankAsString = ss1.str();
+			std::string outputFilenameColorRatio = OUTPUT_PATH + OUTPUT_TAG + "mostdiff_colorratio_rank" + numPixelsToRankAsString;
+			// if(SQUARE_SCORE){
+			// 	outputFilenameColorRatio += "_squarescore"; 
+			// }
+			std::stringstream ss2;
+			ss2 << powersOfScore[score_power_index];
+			std::string powerAsString = ss2.str();
+			outputFilenameColorRatio += "_power" + powerAsString;
+			if(INVERT_SCORES){
+				outputFilenameColorRatio += "_invertscore";
+			}
+			outputFilenameColorRatio += ".ppm";
+			writeImage(mostDifferentImage_colorRatio, outputFilenameColorRatio);
+			deleteImage(mostDifferentImage_colorRatio);
+		}
+	}	
+
+
 	writeImage(mostDifferentImage_regular, OUTPUT_PATH + OUTPUT_TAG + "mostdiff_regular.ppm");
-	writeImage(mostDifferentImage_colorRatio, OUTPUT_PATH + OUTPUT_TAG + "mostdiff_colorratio.ppm");
+	
 	writeImage(mostDifferentImage_Bright, OUTPUT_PATH + OUTPUT_TAG + "mostdiff_brightness.ppm");
 	writeImage(mostDifferentImage_Combined, OUTPUT_PATH + OUTPUT_TAG + "mostdiff_combined_weighed4.ppm");  //easymath, hardmath, and weighed1.
 
 	deleteImage(mostDifferentImage_regular);
-	deleteImage(mostDifferentImage_colorRatio);
 	deleteImage(mostDifferentImage_Bright);
 	deleteImage(mostDifferentImage_Combined);
 
