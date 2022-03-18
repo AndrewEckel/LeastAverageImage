21eafbe085b2cefffef8f3b71c3c84c38bbe5af0
Andrew Eckel <ALEckel@gmail.com>
2022-03-18 11:33:46 -0400

    Removed the batch file for creating a 64 bit static version, because it created a non-working EXE.

--- program/make_64bit_static_version.bat
+++
@@ -1,3 +0,0 @@
-rem This is a batch file for a 64bit Windows machine. It compiles a 64 bit static executable, without using MinGW or the makefile at all.
-del lai.exe
-g++ ../src/* -std=c++11 -static -flto -o lai.exe
